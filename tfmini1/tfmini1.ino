01281f9b3ffd64c7622c4bacf28a69b880b6edd2
CoolShablul <Wooowy@gmail.com>
2023-02-28 15:06:48 +0200

    change in data format from arduino to processing

--- tfmini1/tfmini1.ino
+++ tfmini1/tfmini1.ino
@@ -24,50 +24,7 @@ float height = 61;          //height (in cm) of the sensor
 float initDistance;   //expected diagonal distance based on the angle and height
 
 SoftwareSerial SerialTFMini(2, 3);          //The only value that matters here is the first one, 2, Rx
- 
-void getTFminiData(float* distance, float* strength)
-{
-  static char i = 0;
-  char j = 0;
-  int checksum = 0;
-  static int rx[9];
-  if (SerialTFMini.available())
-  {
-    rx[i] = SerialTFMini.read();
-    if (rx[0] != 0x59)
-    {
-      i = 0;
-    }
-    else if (i == 1 && rx[1] != 0x59)
-    {
-      i = 0;
-    }
-    else if (i == 8)
-    {
-      for (j = 0; j < 8; j++)
-      {
-        checksum += rx[j];
-      }
-      if (rx[8] == (checksum % 256))
-      {
-        *distance = rx[2] + rx[3] * 256;
-        *strength = rx[4] + rx[5] * 256;
-      }
-      i = 0;
-    }
-    else
-    {
-      i++;
-    }
-  }
-}
 
-void calculateInitDistance()
-{
-  float angle_rad = (angle * PI) / 180;       //convert degrees to radians
-  initDistance = height * (1 / cos(angle_rad));
-}
- 
 void setup()
 {
   pinMode(redPin, OUTPUT);
@@ -79,8 +36,6 @@ void setup()
   Serial.println ("Initializing...");
   SerialTFMini.begin(TFMINI_BAUDRATE);    //Initialize the data rate for the SoftwareSerial port
   tfmini.begin(&SerialTFMini);            //Initialize the TF Mini sensor
-//  tfmini.sendCommand(SET_BAUD_RATE, BAUD_9600);
-
 
   establishContact();  // send a byte to establish contact until receiver responds
   delay(50);
@@ -130,42 +85,19 @@ void loop()
   {
     getTFminiData(&distance, &strength);
   }
-  
-//  sendData(distance, strength);
-//  Serial.println(counter);
-//  Serial.print("distance: ");
-//  Serial.println(distance);
-//  Serial.print("cm\t");
-//  Serial.print("strength: ");
-//  Serial.println(strength);
+
 
     Serial.println(
       " Distance: " + String(distance) + " cm \n" + 
       "Initial Distance: " + String(initDistance) + " cm \n" +
       "Strength: " + String(strength) + " \n");
-  
-//  for (int i = 31; 0 < i; i--) {        //we shift all vals in cyclicArray to the right
-//    cyclicArray[i] = cyclicArray[i-1];
-//  }
-    
-//  cyclicArray[0] = distance;          //insert distance measurment into first cell of cyclicArray
-//  currentAverage += float(distance >> five);    //LSH 5 = divide by 32  
-
 
   currentAverage += distance / 32;     
   counter++;                          //update counter
        
   if (counter == 32){
       counter = 0;                     //we reset counter to refill cyclicArray once again
-
-
-
-//      Serial.println(
-//        " Distance: " + String(distance) + " cm \n" + 
-//        "Initial Distance: " + String(initDistance) + " cm \n" +
-//        "Strength: " + String(strength) + " cm \n");
- 
-
+      
       if (currentAverage < 10){   //something is very close to the sensor
         analogWrite(redPin,0);
         analogWrite(bluePin,200);
@@ -212,7 +144,6 @@ void loop()
 
 
 void establishContact() {
-//  while (Serial.available() <= 0) {
     delay(100);
     Serial.print('A');   // send a capital A
     delay(300);
@@ -228,10 +159,6 @@ void techMode(){
   while(true){                //enter technician mode loop
     
 
-
-    
-//    Serial.println("skrrr");
-
     String inString = Serial.readString();
     if (inString[0] == 'a') {
        angle = (inString.substring(2)).toInt();
@@ -243,7 +170,6 @@ void techMode(){
       
     else if (inString[0] == 'h') {
        height = (inString.substring(2)).toInt();
-
        delay(100);
        EEPROM.write(1, height);
        delay(100);
@@ -267,34 +193,61 @@ void techMode(){
         while (!Serial);            // wait for serial port to connect. Needed for native USB port only
         getTFminiData(&distance, &strength);    //we read from sensor into distance   
         if (counter == 11520){
-          Serial.println(
-          " Distance: " + String(distance) + " cm \n" + 
-          "Initial Distance: " + String(initDistance) + " cm \n" +
-          "Strength: " + String(strength) + "\n");
+          Serial.println(" D" + String(distance));
+          Serial.println("S" + String(strength));
+          Serial.println("I" + String(initDistance));
+          
           counter = 0;
-
-  //        Serial.print(" Distance: ");
-  //        Serial.print( distance);
-  //        Serial.println(" cm");
-  //        Serial.print(" Initial distance ");
-  //        Serial.print(initDistance);
-  //        Serial.println(" cm");
-  //        Serial.print(" Strength: ");
-  //        Serial.println(strength);
         }
         counter++;
-        
-        }
-//        String x = Serial.readString();
-//        if (inString[0] == 'm'){
-//          break;
-//        }
       }
-
+    }
+  }
+} 
+   
+void calculateInitDistance()
+{
+  float angle_rad = (angle * PI) / 180;       //convert degrees to radians
+  initDistance = height * (1 / cos(angle_rad));
+}
  
-      }
-  }  
 
+void getTFminiData(float* distance, float* strength)
+{
+  static char i = 0;
+  char j = 0;
+  int checksum = 0;
+  static int rx[9];
+  if (SerialTFMini.available())
+  {
+    rx[i] = SerialTFMini.read();
+    if (rx[0] != 0x59)
+    {
+      i = 0;
+    }
+    else if (i == 1 && rx[1] != 0x59)
+    {
+      i = 0;
+    }
+    else if (i == 8)
+    {
+      for (j = 0; j < 8; j++)
+      {
+        checksum += rx[j];
+      }
+      if (rx[8] == (checksum % 256))
+      {
+        *distance = rx[2] + rx[3] * 256;
+        *strength = rx[4] + rx[5] * 256;
+      }
+      i = 0;
+    }
+    else
+    {
+      i++;
+    }
+  }
+}
 
 void getVariables(){
   //get global variables values from EEPROM
